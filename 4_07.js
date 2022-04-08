5c9e28f952923d8a47ea465cf68ebf2399e0d702
Rama Krishna <ramakrishnat@live.com>
2022-04-08 00:09:10 -0400

    javascript practice

---
+++ 4_07.js
@@ -0,0 +1,25 @@
+// console.log('Hello World');
+// let x=4,y=3;
+// z=x+y;
+// console.log(z);
+
+// // Declare and assign values to 10 different variables
+// // Find average of first 5 variables
+// // Find average of last 5 variable
+// // Find average of above two results
+
+// let a=1,b=2,c=3,d=4,e=5,f=6,g=7,h=8,i=9,j=10;
+// x=(a+b+c+d+e)/5
+// y=(f+g+h+i+j)/5
+// z=(x+y)/2
+// console.log("average of first 5 variables is:");console.log(x) ;
+// console.log("average of last 5 variable is:");console.log(y);
+// console.log('average of above 2 results is:');console.log(z);
+
+// // for loop to count from 10 to 1
+for(x=10;x>0;x=x-1){console.log(x)};
+// counting from 1 to 10
+for(x=1;x<5;x=x+1){console.log(x)};
+
+// create a loop that outputs multiples of 3 starting at 6 ending at 60.
+for(x=3;x<60;x=x+3){console.log(x)};
