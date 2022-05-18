11fd4575b6ee1978419f1906fd8f9a343c519405
Rama Krishna <ramakrishnat@live.com>
2022-05-18 00:09:42 -0400

    add

--- 5_13/server.js
+++ 5_13/server.js
@@ -32,6 +32,15 @@ app.get('/tip/:total/:tipPercentage',(req, res)=>{
 
 // ///////////////Magic 8 Ball
 
+app.get('/magic/:question', function(req,res){
+    let answers = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely","You may rely on it", "As I see it yes", "Most likely", "Outlook good","Yes", "Signs point to yes", "Reply hazy try again", "Ask again later","Better not tell you now", "Cannot predict now", "Concentrate and ask again","Don't count on it", "My reply is no", "My sources say no","Outlook not so good", "Very doubtful"]
+    let q = req.params.question;
+    q.replace(/%20/g, " ")
+    let randomNum = Math.floor(Math.random()* answers.length)
+
+    res.send(q + "<h1>" + answers[randomNum] + "</h1>");
+})
+
 // ////////////////
 
 app.listen(3000, () => {
